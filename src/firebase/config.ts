b38790f519c97b8c30721e23896347f070070061
Edwin Aguiar <a.cresidenciaselvallecobranzas@gmail.com>
2025-12-06 02:51:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7121676828-d1908",
+  "appId": "1:771550853612:web:3d1e14d647026ce320e8c9",
+  "apiKey": "AIzaSyCKaFnax6D5M682VNhfLuOFF9IXanEOlco",
+  "authDomain": "studio-7121676828-d1908.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "771550853612"
+};
